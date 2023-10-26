e205e9458ee2d4db2bf4d8fcc776bbd0b960de07
Xander <xander.berten@student.howest.be>
2023-10-26 10:55:14 +0200

    Added more comments to BVH

--- BVHNode.cpp
+++ BVHNode.cpp
@@ -23,16 +23,20 @@ namespace dae
             IntersectBVH( ray, node.firstPrim + 1 , hitRecord);
         }
     }
+    
     void BVH::BuildBVH(const std::vector<TriangleMesh>& triangleMeshes)
     {
         //Get the mesh
         Meshes = triangleMeshes;
 
+
+        //Add the triangle count
         for(const auto& mesh : Meshes)
         {
             amountOfTriangles += static_cast<int>(mesh.GetAmountOfTriangles());
         }
-        
+
+        //Resize nodes and index of triangles
         bvhNode.resize(static_cast<size_t>(amountOfTriangles) * 2);
         triangleIndex.resize(amountOfTriangles);
         
@@ -41,6 +45,7 @@ namespace dae
         {
             triangleIndex[i] = i;
         }
+
         
         // assign all triangles to root node
         BVHNode& root = bvhNode[rootNodeIdx];
@@ -52,6 +57,7 @@ namespace dae
         // subdivide recursively
         Subdivide( rootNodeIdx );
     }
+    
     void BVH::UpdateNodeBounds( int nodeIdx )
     {
         BVHNode& node = bvhNode[nodeIdx];
@@ -74,57 +80,64 @@ namespace dae
     }
     void BVH::Subdivide( int nodeIdx )
     {
-        // terminate recursion
+        // terminate recursion when there is nothing more to split
         BVHNode& node = bvhNode[nodeIdx];
-        
         if (node.triangleCount <= 2) return;
 
         // determine split axis and position
         Vector3 extent = node.aabbMax - node.aabbMin;
-
         int axis = 0;
-
         if (extent.y > extent.x) axis = 1;
         if (extent.z > extent[axis]) axis = 2;
 
+        // Calculate the position of the splitting plane (median of the chosen axis)
         const float splitPos = node.aabbMin[axis] + extent[axis] * 0.5f;
 
-        // in-place partition
-        int i = node.firstPrim;
-        int j = i + node.triangleCount - 1;
+
         
-        while (i <= j)
+        //Left pointer starts at the beginning of the array
+        //Right pointer starts at the end of the array
+        int leftPointer = node.firstPrim;
+        int rightPointer = leftPointer + node.triangleCount - 1;
+        
+        while (leftPointer <= rightPointer)
         {
-            if (GetTriangleByIndex(triangleIndex[i]).center[axis] < splitPos)
+            // Check if the center coordinate of the current triangle is on the left side of the splitting plane
+            if (GetTriangleByIndex(triangleIndex[leftPointer]).center[axis] < splitPos)
             {
-                ++i;
+                ++leftPointer;
             }
             else
             {
-                std::swap( triangleIndex[i], triangleIndex[j--] );
+                // If the center coordinate is on the right side of the splitting plane, swap the triangles
+                // This moves the right-side pointer to the left while maintaining order
+                std::swap( triangleIndex[leftPointer], triangleIndex[rightPointer--] );
             }
                 
         }
         
-        // abort split if one of the sides is empty
-        const int leftCount = i - node.firstPrim;
+        //Abort split if one of the sides is empty
+        const int leftCount = leftPointer - node.firstPrim;
         
         if (leftCount == 0 || leftCount == node.triangleCount) return;
         
-        // create child nodes
+        // Create child nodes for the left and right sub-nodes
         const int leftChildIdx = nodesUsed++;
         const int rightChildIdx = nodesUsed++;
-        
+
+        // Update child node properties
         bvhNode[leftChildIdx].firstPrim = node.firstPrim;
         bvhNode[leftChildIdx].triangleCount = leftCount;
-        bvhNode[rightChildIdx].firstPrim = i;
+        bvhNode[rightChildIdx].firstPrim = leftPointer;
         bvhNode[rightChildIdx].triangleCount = node.triangleCount - leftCount;
         node.firstPrim = leftChildIdx;
         node.triangleCount = 0;
+        
+        // Update the bounding boxes of child nodes
         UpdateNodeBounds( leftChildIdx );
         UpdateNodeBounds( rightChildIdx );
 
-        // Subdivide teh left and right childs
+        // Subdivide teh left and right children
         Subdivide( leftChildIdx );
         Subdivide( rightChildIdx );
     }
